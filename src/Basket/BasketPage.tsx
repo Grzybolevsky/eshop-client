cb6f6eb01e1524a6797f330c5f8b87ccad95e786
Szymon Żak <szymon436@gmail.com>
2022-09-20 09:52:45 +0200

    Added Hooks for fetching data.

--- src/Basket/BasketPage.tsx
+++ src/Basket/BasketPage.tsx
@@ -1,3 +1,48 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { Table, TableBody, TableCell, TableHead, TableRow } from "@mui/material";
+import BasketProduct from "./BasketProduct";
+
 export default function BasketPage() {
-  return <></>;
+  const [basketProducts, setBasketProducts] = useState<BasketProduct[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  useEffect(() => {
+    setIsLoading(true);
+    axios.get(`https://ebusiness-eshop-api.azurewebsites.net/basket`).then(
+      (response) => {
+        setBasketProducts(response.data);
+        setIsLoading(false);
+      },
+      (err) => setError(err),
+    );
+  }, []);
+  return (
+    <>
+      {isLoading
+        ? "Loading..."
+        : !error && (
+            <Table>
+              <TableHead>
+                <TableRow>
+                  <TableCell>Id zamówienia</TableCell>
+                  <TableCell>Produkt</TableCell>
+                  <TableCell>Liczba</TableCell>
+                  <TableCell>Całkowita cena</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {basketProducts.map((basketProduct) => (
+                  <TableRow key={basketProduct.id}>
+                    <TableCell>{basketProduct.id}</TableCell>
+                    <TableCell>{basketProduct.product.toString()}</TableCell>
+                    <TableCell>{basketProduct.quantity}</TableCell>
+                    <TableCell>{basketProduct.totalPrice}</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          )}
+    </>
+  );
 }
--- src/Order/OrdersPage.tsx
+++ src/Order/OrdersPage.tsx
@@ -1,7 +1,45 @@
+import { useEffect, useState } from "react";
+import Order from "./Order";
+import axios from "axios";
+import { Table, TableBody, TableCell, TableHead, TableRow } from "@mui/material";
+
 export default function OrdersPage() {
+  const [orders, setOrders] = useState<Order[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  useEffect(() => {
+    setIsLoading(true);
+    axios.get(`https://ebusiness-eshop-api.azurewebsites.net/orders`).then(
+      (response) => {
+        setOrders(response.data);
+        setIsLoading(false);
+      },
+      (err) => setError(err),
+    );
+  }, []);
   return (
     <>
       <h2>Zamówienia</h2>
+        {isLoading
+          ? "Loading..."
+          : !error && (
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>Id zamówienia</TableCell>
+                <TableCell>Data zamówienia</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {orders.map((order) => (
+                <TableRow key={order.id}>
+                  <TableCell>{order.id}</TableCell>
+                  <TableCell>{order.createdAt.toString()}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        )}
     </>
   );
 }
